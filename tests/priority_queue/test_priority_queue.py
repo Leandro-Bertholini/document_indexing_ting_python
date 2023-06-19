7d9d44a4e173f7611eb2206b693190746a10062f
Leandro Bertholini <bertholinileandro1@gmail.com>
2023-06-19 13:10:35 -0300

    feat: teste para a função de priorização da fila

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,22 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    has_priority = PriorityQueue()
+
+    # Teste de prioridade na fila, retornando o valor correto
+    has_priority.enqueue({"qtd_linhas": 3})
+    has_priority.enqueue({"qtd_linhas": 4})
+    has_priority.enqueue({"qtd_linhas": 2})
+    has_priority.enqueue({"qtd_linhas": 10})
+    has_priority.enqueue({"qtd_linhas": 15})
+
+    assert has_priority.dequeue() == {"qtd_linhas": 3}
+
+    assert has_priority.search(0) == {"qtd_linhas": 2}
+
+    with pytest.raises(IndexError) as e:
+        has_priority.search(1)
+
+    assert str(e.value) == "Índice Inválido ou Inexistente"
