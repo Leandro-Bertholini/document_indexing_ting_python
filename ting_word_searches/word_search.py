0939fff97f4ab375bdbf99b82ee924ec74fbcb60
Leandro Bertholini <bertholinileandro1@gmail.com>
2023-06-19 17:37:06 -0300

    feat: criação da função exists_word. Req_7_done

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    found_files = []
 
+    for i in range(len(instance)):
+        word_rows = []
+        for e, line in enumerate(instance.search(i)["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                word_rows.append({"linha": e + 1})
+
+        if word_rows:
+            found_files.append({
+        "palavra": word,
+        "arquivo": instance.search(i)['nome_do_arquivo'],
+        "ocorrencias": word_rows,
+    })
+
+    return found_files
 
 def search_by_word(word, instance):
     """Aqui irá sua implementação"""
